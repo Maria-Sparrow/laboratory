196bf9c6e663a23866460fc73fcf969d85c65454
Maria Sapiy <mariasapiy999@gmail.com>
2020-01-23 15:21:37 +0200

    Rewrote main file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+package ua.lviv.iot.first.lab;
+
 public class Main {
     public static void main(String[] args) {
+
         PromotionalGoods Xs = new PromotionalGoods();
         PromotionalGoods XPro = new PromotionalGoods(45, "iPhone", 12000, 6600);
-        PromotionalGoods SPlus = new PromotionalGoods(30, "iPhone", 10000, 7000, 14353, "headset", "metal", 5.5, 113456789);
+        PromotionalGoods SPlus = new PromotionalGoods(30, "iPhone", 10000, 7000, 14353, "headset", "metal", 5.5);
 
         Xs.toString();
-        System.out.println("\n\n\n\n\n");
+        System.out.println(Xs);
         XPro.toString();
-        System.out.println("\n\n\n\n\n");
+        System.out.println(XPro);
         SPlus.toString();
-        System.out.println("\n\n\n\n\n");
+        System.out.println(SPlus);
 
         PromotionalGoods[] arrayOfPromotionalGoods = new PromotionalGoods[4];
         int something = 0;
@@ -18,9 +21,7 @@ public class Main {
             something++;
         }
         while (something < 4);
-            for(PromotionalGoods i:arrayOfPromotionalGoods  ){
-                i.toString();
-                System.out.println("\n");
-            }
-    }
-}
+        for(PromotionalGoods i : arrayOfPromotionalGoods ) {
+            i.toString();
+            System.out.println(i);
+        }
